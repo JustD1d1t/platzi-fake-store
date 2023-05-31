81b86be124e506491d0be008ac889fe725844fcb
JustD1d1t <starzyk.dawid.0826@gmail.com>
2023-05-31 19:35:21 +0200

    user-promocodes

---
+++ src/components/user/promocodes/promocode-item.vue
@@ -0,0 +1,55 @@
+<template>
+    <box class="promocode-item">
+        <p class="promocode-item__title">{{ promocode.name }}</p>
+        <p class="promocode-item__code">
+            <b>code:</b> {{ promocode.code }}
+        </p>
+        <p class="promocode-item__discount">
+            <b>discount:</b> {{ promocode.discount * 100 }}%
+        </p>
+        <p class="promocode-item__condition">
+            <b>conditions:</b> {{ promocode.condition }}
+        </p>
+        <button class="btn btn-primary promocode-item__button" :class="{ disabled: promocodeUsed }"
+            @click="this.$emit('setActivePromocode', promocode.code)">Use promocode</button>
+    </box>
+</template>
+
+<script>
+export default {
+    props: {
+        promocode: {
+            type: Object,
+            default: () => { }
+        },
+        activePromocode: {
+            type: String,
+            default: ''
+        }
+    },
+    computed: {
+        promocodeUsed() {
+            return this.activePromocode === this.promocode.code
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.promocode-item {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    &__title {
+        font-size: 24px;
+        font-weight: 700;
+        margin-top: 0;
+    }
+
+    &__button {
+        margin-left: auto !important;
+    }
+}
+</style>
---
+++ src/components/user/promocodes/promocodes-container.vue
@@ -0,0 +1,44 @@
+<template>
+    <section class="promocodes">
+        <promocode-item v-for="promocode in promocodes" :promocode="promocode" :key="promocode.code"
+            :activePromocode="activePromocode" @setActivePromocode="this.$emit('setActivePromocode', promocode.code)" />
+    </section>
+</template>
+
+<script>
+import promocodeItem from '@/components/user/promocodes/promocode-item'
+export default {
+    components: {
+        promocodeItem
+    },
+    props: {
+        promocodes: {
+            type: Array,
+            default: () => { }
+        },
+        activePromocode: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.promocodes {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto 1fr 1fr;
+    gap: 20px;
+
+    @media screen and (min-width: 767px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+    @media screen and (min-width: 1023px) {
+        grid-template-columns: repeat(3, 1fr);
+    }
+    @media screen and (min-width: 1199px) {
+        grid-template-columns: repeat(4, 1fr);
+    }
+}
+</style>
