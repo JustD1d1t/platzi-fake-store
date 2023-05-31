98d0c013b29b720772c3fe9146feeec7099736a0
JustD1d1t <starzyk.dawid.0826@gmail.com>
2023-05-31 21:10:32 +0200

    fix number

--- src/components/basket/basket-summary.vue
+++ src/components/basket/basket-summary.vue
@@ -2,7 +2,7 @@
     <div class="basket-summary">
         <div class="basket-summary__top">
             <h3>Total</h3>
-            <h4>{{ totalCost }}</h4>
+            <h4>{{ totalCost.toFixed(2) }}</h4>
         </div>
         <div class="basket-summary__content">
             <basketSummaryItem v-for="scooter in addedToCart" :key="scooter.name" :scooter="scooter" />
